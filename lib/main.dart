f17ee6a43c01473167062e2a2db2adf3f2e3891e
Hamzah Hafiz <hamzah7hafiz@gmail.com>
2024-06-28 10:10:13 +0300

    1.0.6 error seting gradle

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:university_courses/firebase_options.dart';
 import 'package:university_courses/providers.dart';
 import 'package:university_courses/routes.dart';
 
-void main() {
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
